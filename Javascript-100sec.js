6447cc7674479e13949cd42d451a7d80c6b5434d
Himanshi Sharma <himanshiksharma@outlook.com>
2024-12-10 08:42:35 +0000

    Javascript in 100sec

---
+++ Javascript-100sec.js
@@ -0,0 +1,10 @@
+/* 
+# Javascript high-level, single-threaded, garbage-collected, interpreted or JIT(ust-In-Time) complied, 
+  prototype-based, multi-paradigm, dynamic language with a non-blocking event loop.
+# Created in 1995 in just one week by BRENDAN EICH with the goal of adding an easy 
+  to learn scripting language to the netscape browser it was originally named mocha.
+# Atwood's Law- Any application that can be written in JavaScript, will eventually be written in JavaScript
+# it's a single threaded language made possible by a non-blocking event loop that can queue up work in the 
+  background without blocking the main thread 
+# javascript is often used to grab an element from the dom document 
+*/
