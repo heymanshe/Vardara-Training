f6272162ed5f1691e17ee1f7b984ac9ce2de3c79
heymanshe <himanshiksharma@outlook.com>
2024-12-11 08:22:04 +0000

    Numbers

---
+++ 03-Numbers.js
@@ -0,0 +1,23 @@
+const score = 400 
+// console.log(score);
+// console.log(typeof score);    
+
+const balance = new Number(100)
+// console.log(balance);
+// console.log(balance.toString().length); //there are other properties too, that can be used with this method
+// console.log(balance.toFixed(2));
+//console.log(typeof balance);    // Output: "object"
+
+//returns a string
+const otherNumber = 23.8755
+// console.log(otherNumber.toPrecision(3));
+
+const exampleNumber = 123.8755
+// console.log(exampleNumber.toPrecision(3));
+
+const exampleNumberTwo = 1123.8755
+// console.log(exampleNumberTwo.toPrecision(3));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString());  //follows US digits system
+// console.log(hundreds.toLocaleString('en-IN'));
