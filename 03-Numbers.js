037b656f26bceb27b5707c41a701fc8d6e86568f
heymanshe <himanshiksharma@outlook.com>
2024-12-12 06:19:55 +0000

    Number Properties

--- 03-Numbers.js
+++ 03-Numbers.js
@@ -21,3 +21,24 @@ const exampleNumberTwo = 1123.8755
 const hundreds = 1000000
 // console.log(hundreds.toLocaleString());  //follows US digits system
 // console.log(hundreds.toLocaleString('en-IN'));
+
+
+
+//###################### Number Propertites #######################
+
+/*
+EPSILON	The difference between 1 and the smallest number > 1.
+MAX_VALUE	The largest number possible in JavaScript
+MIN_VALUE	The smallest number possible in JavaScript
+MAX_SAFE_INTEGER	The maximum safe integer (253 - 1)
+MIN_SAFE_INTEGER	The minimum safe integer -(253 - 1)
+POSITIVE_INFINITY	Infinity (returned on overflow)
+NEGATIVE_INFINITY	Negative infinity (returned on overflow)
+NaN	A "Not-a-Number" value
+*/
+
+// Number properties belong to the JavaScript Number Object. Cannot be used on number variables
+let x = 6;
+console.log(x.MAX_VALUE);
+let y = Number.MAX_VALUE;
+console.log(Number.MAX_VALUE);
